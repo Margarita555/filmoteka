44e2c1b2d7b25379a77a8fd03097e9bb7f5971e4
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-11-07 21:47:32 +0200

    Update api-service.js

--- src/js/API/api-service.js
+++ src/js/API/api-service.js
@@ -5,12 +5,8 @@ const axios = require('axios').default;
 axios.defaults.baseURL = ACCESS.BASE_URL;
 axios.defaults.headers.common.Authorization = ACCESS.AUTH_TOKEN;
 
-export default class API {
-  constructor() {
-    this.searchQuery = '';
-    this.movieId = '';
-  }
-
+export default {
+  // Получение полной информации о трендах
   async fetchMovieTrending() {
     try {
       const response = await axios.get('/trending/movie/day?');
@@ -18,39 +14,73 @@ export default class API {
     } catch (error) {
       console.error(error);
     }
-  }
-
-  async fetchMovieSearchQuery() {
+  },
+  // Фетч по поисковому запросу
+  async fetchMovieSearchQuery(searchQuery) {
     try {
-      const response = await axios.get(`/search/movie?&query=${this.searchQuery}`);
+      const response = await axios.get(`/search/movie?&query=${searchQuery}`);
       return response.data.results;
     } catch (error) {
       console.error(error);
     }
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-
-  async fetchMovieDescription() {
+  },
+  // Фетч описания фильма по его ID
+  async fetchMovieDescription(movieId) {
     try {
-      const response = await axios.get(`/movie/${this.movieId}?`);
+      const response = await axios.get(`/movie/${movieId}?`);
       return response.data;
     } catch (error) {
       console.error(error);
     }
-  }
+  },
+};
 
-  get id() {
-    return this.movieId;
-  }
+// export default class API {
+//   constructor() {
+//     this.searchQuery = '';
+//     this.movieId = '';
+//   }
 
-  set id(newMovieId) {
-    this.movieId = newMovieId;
-  }
-}
+//   async fetchMovieTrending() {
+//     try {
+//       const response = await axios.get('/trending/movie/day?');
+//       return response.data.results;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   async fetchMovieSearchQuery() {
+//     try {
+//       const response = await axios.get(`/search/movie?&query=${this.searchQuery}`);
+//       return response.data.results;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+
+//   async fetchMovieDescription() {
+//     try {
+//       const response = await axios.get(`/movie/${this.movieId}?`);
+//       return response.data;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   get id() {
+//     return this.movieId;
+//   }
+
+//   set id(newMovieId) {
+//     this.movieId = newMovieId;
+//   }
+// }
