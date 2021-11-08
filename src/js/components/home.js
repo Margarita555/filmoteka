ff545cdbb591b2df2057b5e929b8078e3ad077c6
Yevhen-Karpov <81827609+Yevhen-Karpov@users.noreply.github.com>
2021-11-08 21:10:31 +0200

    08.11 (#29)

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -2,14 +2,16 @@ import home from '../API/api-service';
 import getRefs from '../refs/get-refs';
 import card from '../../handlebars/cardMovie.hbs';
 
-const { insertPoint } = getRefs();
-const { fetchMovieTrending} = home;
+const { insertPoint, homeLink } = getRefs();
+const { fetchMovieTrending } = home;
+console.log(homeLink);
 
-fetchMovieTrending().then((result) => {
-   insertPoint.insertAdjacentHTML('beforeend', card(result))
-   console.log(result);
- })
- .catch( (err) => {
-   console.log(err);
- })
-
+fetchMovieTrending()
+  .then(result => {
+    insertPoint.insertAdjacentHTML('beforeend', card(result));
+    homeLink.classList.add('active');
+    console.log(result);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- src/js/refs/get-refs.js
+++ src/js/refs/get-refs.js
@@ -2,6 +2,7 @@ export default function getRefs() {
   return {
     searchInputRef: document.querySelector('.header__search-form-input'),
     searchBtnRef: document.querySelector('.header__form-btn'),
-    insertPoint: document.querySelector('.hero__list')
+    insertPoint: document.querySelector('.hero__list'),
+    homeLink: document.querySelector('#home-link'),
   };
 }
