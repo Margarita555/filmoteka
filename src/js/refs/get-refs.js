3de28c7ff32003282ccadbb7918d49d9686cef7d
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-11-08 12:58:57 +0200

    main section (#21)

--- src/js/refs/get-refs.js
+++ src/js/refs/get-refs.js
@@ -2,5 +2,6 @@ export default function getRefs() {
   return {
     searchInputRef: document.querySelector('.header__search-form-input'),
     searchBtnRef: document.querySelector('.header__form-btn'),
+    insertPoint: document.querySelector('.hero__list')
   };
 }
