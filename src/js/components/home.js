b2d149e941cd33eecdbdc552206c76c4364e29c9
NataliyaMylyan <mnatal2015@gmail.com>
2021-11-11 18:45:31 +0200

    feature/spinner/backToTop

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -2,19 +2,21 @@ import API from '../API/api-service';
 import getRefs from '../refs/get-refs';
 import card from '../../handlebars/cardMovie.hbs';
 import createCardData from './create-card-data';
+import { startSpinner, stopSpinner } from './spinner.js';
 
 const { insertPoint, homeLink, libraryLink, headerForm, headerButton } = getRefs();
 
 const api = new API();
 
 async function createMarkup() {
+  startSpinner();
   try {
     const data = await api.fetchMovieTrending();
     const result = await data.results;
     const markup = await createCardData(result);
 
     insertPoint.insertAdjacentHTML('beforeend', card(markup));
-
+    stopSpinner();
     homeLink.classList.add('active');
     libraryLink.classList.remove('active');
     headerForm.classList.remove('disabled');
