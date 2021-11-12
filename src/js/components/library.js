9742459c915db153b68476101de11f2a1f4ad2d5
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2021-11-12 17:35:12 +0200

    lib-fix (#76)

--- src/js/components/library.js
+++ src/js/components/library.js
@@ -1,5 +1,6 @@
 import getRefs from '../refs/get-refs';
-import card from '../../handlebars/cardForLibrary.hbs';
+import card from '../../handlebars/cardMovie.hbs';
+
 const {
   libraryLink,
   homeLink,
@@ -12,7 +13,7 @@ const {
 } = getRefs();
 
 libraryLink.addEventListener('click', openLibrary);
-function openLibrary(e) {
+function openLibrary() {
   insertPoint.innerHTML = '';
   header.classList.replace('header__background-home', 'header__background-library');
   homeLink.classList.remove('active');
@@ -20,15 +21,15 @@ function openLibrary(e) {
   headerForm.classList.add('disabled');
   headerButton.classList.remove('disabled');
 }
-function watchedStorage() {
+  async function watchedStorage() {
   btnQueue.classList.remove('in-active');
   btnWatched.classList.add('in-active');
   insertPoint.innerHTML = '';
   let items = JSON.parse(localStorage.getItem('Watched'));
   console.log(items);
-
   insertPoint.insertAdjacentHTML('beforeend', card(items));
 }
+
 function queuedStorage() {
   btnWatched.classList.remove('in-active');
   btnQueue.classList.add('in-active');
