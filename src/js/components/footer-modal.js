1f0e3d53539195328ce74e8d00e1871178c5d2a8
Peleshko <berdyanskpel6@gmail.com>
2021-11-14 23:01:52 +0200

    add wrapper-card

--- src/js/components/footer-modal.js
+++ src/js/components/footer-modal.js
@@ -2,11 +2,24 @@ import getRefs from "../refs/get-refs";
 
 (() => {
   const  { openFooterModalBtn, closeFooterModalBtn, footerModal, backdropRef } = getRefs();
-  openFooterModalBtn.addEventListener('click', toggleModal);
-  closeFooterModalBtn.addEventListener('click', toggleModal);
- // backdropRef.addEventListener('click', toggleModal);
+ 
+ 
+ 
+ openFooterModalBtn.addEventListener('click', toggleModal);
+ closeFooterModalBtn.addEventListener('click', toggleModal);
+
+ window.onclick = function (e) {
+   if (e.target == footerModal) {
+    footerModal.classList.toggle('is-hidden');
+   }
+  }
+
+  window.addEventListener('keydown', e => {
+    if (e.code === 'Escape') toggleModal();
+  });
 
   function toggleModal () {
-      footerModal.classList.toggle('is-hidden');
-  }
+    footerModal.classList.toggle('is-hidden');
+ }
+
 })();
