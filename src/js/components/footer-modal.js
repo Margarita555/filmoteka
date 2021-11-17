380bf97fd37207a172f032a62ccf39a1de9f7b58
Yevhen-Karpov <goldblogart@gmail.com>
2021-11-17 15:52:52 +0200

    17
    switch

--- src/js/components/footer-modal.js
+++ src/js/components/footer-modal.js
@@ -1,24 +1,22 @@
-import getRefs from "../refs/get-refs";
+import getRefs from '../refs/get-refs';
 
 (() => {
-  const  { openFooterModalBtn, closeFooterModalBtn, footerModal } = getRefs();
- 
- 
- 
- openFooterModalBtn.addEventListener('click', toggleModal);
- closeFooterModalBtn.addEventListener('click', toggleModal);
+  const { openFooterModalBtn, closeFooterModalBtn, footerModal, themeSwitch } = getRefs();
 
- window.onclick = function (e) {
-   if (e.target == footerModal) {
+  openFooterModalBtn.addEventListener('click', toggleModal);
+  closeFooterModalBtn.addEventListener('click', toggleModal);
+
+  window.onclick = function (e) {
+    if (e.target == footerModal) {
+      footerModal.classList.toggle('is-hidden');
+    }
+    if (e.code === 'Escape') {
+      footerModal.classList.toggle('is-hidden');
+    }
+  };
+
+  function toggleModal() {
     footerModal.classList.toggle('is-hidden');
-   }
-   if (e.code === 'Escape') {
-    footerModal.classList.toggle('is-hidden');
-   }
+    themeSwitch.classList.toggle('disabled');
   }
-
-  function toggleModal () {
-    footerModal.classList.toggle('is-hidden');
- }
-
 })();
