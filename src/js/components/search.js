df0ba2553b7d6bafd55ae745ef4cd56c99def565
Volodymyr Klochan <vovkis13@gmail.com>
2021-11-17 22:41:57 +0200

    bugfix4

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -13,31 +13,23 @@ searchForm.addEventListener('submit', onSearchInput);
 
 async function onSearchInput(e) {
   e.preventDefault();
-
   const value = e.currentTarget.elements.query.value;
   if (!value.trim()) return;
   initialReset();
 
   try {
     api._setQuery(value);
-
     startSpinner();
-
     const data = await api.fetchMovieSearchQuery();
     const result = await data.results;
-
     const markup = await createCardData(result);
-
     if (!result.length) {
       searchErr(true);
       stopSpinner();
       return;
     }
     insertPoint.insertAdjacentHTML('beforeend', card(markup));
-
-    let request = "searchQuery";
-    renderPagination(request, data.total_pages, value);
-
+    renderPagination('searchQuery', data.total_pages, value);
     stopSpinner();
   } catch (error) {
     console.error(error);
