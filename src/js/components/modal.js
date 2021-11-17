f8ee430737a7bfba7d3825debc57b7519242c3a2
Volodymyr Klochan <vovkis13@gmail.com>
2021-11-17 19:46:28 +0200

    bugfix

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -41,9 +41,7 @@ async function onClickOnCard(e) {
 }
 
 function onModalBtnClick(e) {
-  e.preventDefault();
   if (e.target.nodeName === 'BUTTON') {
-    console.log(e.target);
     if (e.target.classList.contains('btn--active'))
       deleteItemFromLibrary(e.target.dataset.lib, movieID);
     else addItemToLibrary(e.target.dataset.lib);
