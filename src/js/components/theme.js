fd9db5ebbaa67b79f8014555a3650aaac1a77d42
Yevhen-Karpov <goldblogart@gmail.com>
2021-11-18 17:47:31 +0200

    18
    theme

--- src/js/components/theme.js
+++ src/js/components/theme.js
@@ -5,13 +5,16 @@ const Theme = {
 };
 const { switchItem, selector, bodyContainer } = getRefs();
 const themeInLocal = localStorage.getItem('theme');
-
+const footerTheme = document.querySelector('.footer');
+console.log(footerTheme);
 selector.addEventListener('click', switchTemes);
 selector.addEventListener('click', setLocalStorage);
 
 function switchTemes(e) {
   bodyContainer.classList.toggle(Theme.LIGHT);
   bodyContainer.classList.toggle(Theme.DARK);
+  footerTheme.classList.toggle(Theme.LIGHT);
+  footerTheme.classList.toggle(Theme.DARK);
 }
 
 function setLocalStorage(evt) {
